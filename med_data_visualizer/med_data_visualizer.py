9a261fab0e2fb8244fb8a9412c78f8217a610ef5
Gedis-Nov <gedisnov@gmail.com>
2023-10-20 22:54:11 +0200

    Updated med_data_visualizer.py b|med_data_visualizer_v2

--- med_data_visualizer/med_data_visualizer.py
+++ med_data_visualizer/med_data_visualizer.py
@@ -6,59 +6,54 @@ import numpy as np
 tab = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-tab['overweight'] = tab['weight'] / (tab['height'] / 100) **2
-tab.loc[(tab['overweight'] <= 25), 'overweight'] = 0
-tab.loc[(tab['overweight'] > 25), 'overweight'] = 1
-tab['overweight'] = tab['overweight'].astype(int)
+bmi = tab['weight'] / np.square(tab['height'] / 100)
+tab['overweight'] = (bmi > 25).astype('uint8')
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-tab.loc[tab['cholesterol'] == 1,'cholesterol'] = 0
-tab.loc[tab['gluc'] == 1, 'gluc'] = 0
-tab.loc[tab['cholesterol'] > 1,'cholesterol'] = 1
-tab.loc[tab['gluc'] > 1, 'gluc'] = 1
+tab['cholesterol'] = (tab['cholesterol'] != 1).astype('uint8')
+tab['gluc'] = (tab['gluc'] != 1).astype('uint8')
 
-# Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    q3 = tab.melt(id_vars='cardio',value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
+    melt_tab = tab.melt(id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    #df_cat = None
+    melt_tab = melt_tab.reset_index() \
+                .groupby(['variable', 'cardio', 'value']) \
+                .agg('count') \
+                .rename(columns={'index': 'total'}) \
+                .reset_index()
 
     # Draw the catplot with 'sns.catplot()'
-    ax1 = sns.catplot(col='cardio', x='variable', hue='value', kind='count', data=q3)
-    ax1.set(ylabel='total')
-
-
-    # Get the figure for the output
-    print(ax)
+    ax = sns.catplot(x='variable', col='cardio',  hue='value', kind='count', data=melt_tab)
+    ax.set(ylabel='total')
 
     # Do not modify the next two lines
-    ax1.savefig('catplot.png')
+    ax.savefig('catplot.png')
     return ax
 
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    ctab = tab.copy()
-    ctab = ctab[ctab['ap_lo'] <= ctab['ap_hi']]
-    ctab = ctab[ctab['height'] >= ctab['height'].quantile(0.025)]
-    ctab = ctab[ctab['height'] <= ctab['height'].quantile(0.975)]
-    ctab = ctab[ctab['weight'] >= ctab['weight'].quantile(0.025)]
-    ctab = ctab[ctab['weight'] <= ctab['weight'].quantile(0.975)]
+    heat_tab = tab[
+        (tab['ap_lo'] <= tab['ap_hi'])
+        & (tab['height'] >= tab['height'].quantile(0.025))
+        & (tab['height'] <= tab['height'].quantile(0.975))
+        & (tab['weight'] >= tab['weight'].quantile(0.025))
+        & (tab['weight'] <= tab['weight'].quantile(0.975))]
 
     # Calculate the correlation matrix
-    cor_ctab = ctab.corr()
+    corr = heat_tab.corr()
 
     # Generate a mask for the upper triangle
-    tri_mask = np.triu(np.ones_like(cor_ctab))
+    tri_mask = np.triu(np.ones_like(corr))
 
     # Set up the matplotlib figure
+    fig = plt.figure(figsize=(12,6))
 
     # Draw the heatmap with 'sns.heatmap()'
-    bx = sns.heatmap(cor_ctab, linewidths=0.5, mask=tri_mask, annot=True, annot_kws={'size':5})
-    print(bx)
+    sns.heatmap(corr, mask=tri_mask, annot=True, fmt='.1f', center=0, vmin=0.5, vmax=0.5)
 
     # Do not modify the next two lines
-    bx.savefig('heatmap.png')
-    return bx
+    fig.savefig('heatmap.png')
+    return fig
--- med_data_visualizer/med_data_visualizer_main.py
+++ med_data_visualizer/med_data_visualizer_main.py
@@ -1,14 +1,13 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import clone
-#import med_data_visualizer
+import med_data_visualizer
 from unittest import main
 
 # Test your function by calling it here
-print(clone.draw_cat_plot())
-print(clone.draw_heat_map())
+#print(clone.draw_cat_plot())
+#print(clone.draw_heat_map())
 
 #med_data_visualizer.draw_cat_plot()
 #med_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-#main(module='med_data_visualizer_test', exit=False)
+main(module='med_data_visualizer_test', exit=False)
